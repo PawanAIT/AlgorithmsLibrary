7724edd4693a0e90552969be25b1106217f3ce94
Pawan <pawan161096@gmail.com>
2018-01-07 05:21:15 +0530

    Create Check for BST .cpp

---
+++ Check for BST .cpp
@@ -0,0 +1,17 @@
+Node *p = NULL;
+bool f = true;
+void check(Node *root){
+    if(root == NULL)
+        return;
+    check(root -> left);
+    if(p!=NULL && p -> data > root -> data)
+        f = false;
+    p = root;
+    check(root -> right);
+}
+bool isBST(Node* root) {
+    f = true;
+    p = NULL;
+    check(root);
+    return f;
+}
