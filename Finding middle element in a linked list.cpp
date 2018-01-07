3a9aae57590f09496dd6740a5ab9a7f91222818e
Pawan <pawan161096@gmail.com>
2018-01-07 06:21:04 +0530

    Create Finding middle element in a linked list.cpp

---
+++ Finding middle element in a linked list.cpp
@@ -0,0 +1,9 @@
+int getMiddle(Node *head)
+{
+   Node *slow = head , *fast = head;
+   while(fast != NULL && fast -> next != NULL){
+       slow = slow -> next;
+       fast = fast -> next -> next;
+   }
+   return slow -> data;
+}
