aea9a7d2beec4a0e4077e69b18dbd0f174b28782
Pawan <pawan161096@gmail.com>
2018-02-22 23:19:07 +0530

    Update FFT.cpp

--- FFT.cpp
+++ FFT.cpp
@@ -1,54 +1,76 @@
 #include <bits/stdc++.h>
 using namespace std;
 const double PI = acos(-1);
-const int N = 1000 + 5;
- 
 typedef complex<double> base;
 vector<base> omega;
-int FFT_N ;
+long long  FFT_N ;
  
-void init_fft(int n)
+void init_fft(long long  n)
 {
   FFT_N  = n;
   omega.resize(n);
   double angle = 2 * PI / n;
-  for(int i = 0; i < n; i++)
+  for(long long  i = 0; i < n; i++)
     omega[i] = base( cos(i * angle), sin(i * angle));
 }
 void fft (vector<base> & a)
 {
-  int n = (int) a.size();
+  long long  n = (long long ) a.size();
   if (n == 1)  return;
-  int half = n >> 1;
+  long long  half = n >> 1;
   vector<base> even (half),  odd (half);
-  for (int i=0, j=0; i<n; i+=2, ++j)
+  for (long long  i=0, j=0; i<n; i+=2, ++j)
     {
       even[j] = a[i];
       odd[j] = a[i+1];
     }
   fft (even), fft (odd);
-  for (int i=0, fact = FFT_N/n; i < half; ++i)
+  for (long long  i=0, fact = FFT_N/n; i < half; ++i)
     {
       base twiddle =  odd[i] * omega[i * fact] ;
       a[i] =  even[i] + twiddle;
       a[i+half] = even[i] - twiddle;
     }
 }
-void multiply (const vector<int> & a, const vector<int> & b, vector<int> & res)
+void multiply (const vector<long long > & a, const vector<long long > & b, vector<long long > & res)
 {
   vector<base> fa (a.begin(), a.end()),  fb (b.begin(), b.end());
-  int n = 1;
+  long long  n = 1;
   while (n < 2*max (a.size(), b.size()))  n <<= 1;
   fa.resize (n),  fb.resize (n);
  
   init_fft(n);
   fft (fa),  fft (fb);
-  for (int i=0; i< n; ++i)
+  for (long long  i=0; i< n; ++i)
     fa[i] = conj( fa[i] * fb[i]);
   fft (fa);
   res.resize (n);
-  for (int i=0; i<n; ++i){
-      res[i] = (int) (fa[i].real() / n + 0.5);
-      res[i] = min(res[i] , 1);
+  for (long long  i=0; i<n; ++i){
+      res[i] = (long long ) (fa[i].real() / n + 0.5);
+      res[i] = res[i] ;
     }
 }
+ 
+int  main(){
+    long long  t = 1;
+    scanf("%lld", &t);
+    while(t--){
+         long long  n , k , a , b , c,temp;
+         scanf("%lld %lld %lld %lld" ,&n , &a , &b , &c);
+         vector<long long > alpha(1 , 1), beta(1, 1) , gamma(1 ,1) , ans(1 , 1); 
+         for(long long  i = 1 ; i <=a; i++)
+            alpha.push_back(1);
+         for(long long  i = 1; i <= b;i++)
+           beta.push_back(1);
+       for(long long  i = 1; i <= c;i++)
+           gamma.push_back(1);
+    
+           multiply(alpha , beta , ans);        
+           multiply(ans , gamma , ans);
+            long long res = 0;
+         for(long long  i = 0; i <= min(n, a + b + c) ;i++)
+             res += ans[i];
+           printf("%lld\n", res);
+    }
+      
+} 
