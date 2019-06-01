ed66155fb1dfaf3dbb1d8500d2f92d63d43fb9bd
pawan kumar <pawkumar@microsoft.com>
2019-06-02 03:08:22 +0530

    generateParenthesis

--- Coding/Coding.cpp
+++ Coding/Coding.cpp
@@ -2,28 +2,32 @@
 using namespace std;
 class Solution {
 public:
-	vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
-		sort(
-			people.begin(), people.end(),
-			[](vector<int> & a, vector<int> & b) {
-				if (a[0] != b[0])
-					return a[0] > b[0];
-				return a[1] < b[1];
-			}
-		);
-		vector<vector<int>> ans;
-		for (auto x : people) {
-			ans.insert(ans.begin() + x[1], x);
-		}
+	vector<string> ans;
+	vector<string> generateParenthesis(int n) {
+		int l(n), r(n);
+		string s = "";
+		generate(l, r, s);
 		return ans;
+	}
+	void generate(int l, int r, string s) {
+		if (l == 0 and r == 0) {
+			ans.push_back(s);
+		}
+		if (l <= r) {
+			if (l) {
+				generate(l - 1, r, s + '(');
+			}
+
+			if (r) {
+				generate(l, r - 1, s + ')');
+			}
+		}
 	}
 };
 
 int main() {
 	Solution solution;
-	int target = 0;
-	vector<vector<int>> v{ {7, 0}, {4, 4}, {7, 1}, {5, 0}, {6, 1}, {5, 2} };
-	
-	for (auto i : solution.reconstructQueue(v))
-		cout << i[0] << i[1] << endl;
+	for (auto i : solution.generateParenthesis(3)) {
+		cout << i << endl;
+	}		
 }
