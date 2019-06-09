386d7e7e14cb67a142646b9844ecdc2dd900b666
pawan kumar <pawkumar@microsoft.com>
2019-06-09 11:58:42 +0530

    longest common subsequence.

--- Coding/Coding.cpp
+++ Coding/Coding.cpp
@@ -1,37 +1,32 @@
+// https://leetcode.com/problems/delete-operation-for-two-strings/
 #include "bits/stdc++.h"
+#pragma warning(disable:4996)
+
 using namespace std;
 const int N = 1000 + 5;
 int dp[N][N];
 class Solution {
 public:
-	int Knapsack(vector<int>& weight, vector<int>& values, int capacity) {
+	int minDistance(string word1, string word2) {
 		memset(dp, 0, N * N);
-		for (int i = 1; i <= weight.size(); i++) {
-			for (int j = 1; j <= capacity; j++) {
-				if (j >= weight[i - 1])
-					dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - weight[i - 1]] + values[i - 1]);
-				else
-					dp[i][j] = dp[i - 1][j];
+		for (int i = 1; i <= word1.size(); i++) {
+			for (int j = 1; j <= word2.size(); j++) {
+				if (word1[i - 1] == word2[j - 1]) {
+					dp[i][j] = 1 + dp[i - 1][j - 1];
+				}
+				else {
+					dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+				}
 			}
 		}
-		return dp[weight.size()][capacity];
+		return word1.size() + word2.size() - 2 * dp[word1.size()][word2.size()];
 	}
 };
 int main() {
-
-	int t;
-	cin >> t;
-	while (t--) {
-		Solution solution;
-		int n, capacity;
-		cin >> n >> capacity;
-		vector<int>	weight(n);
-		vector<int>	values(n);
-		for (auto& i : values)
-			cin >> i;
-		for (auto& i : weight)
-			cin >> i;
-		cout << solution.Knapsack(weight, values, capacity);
-	}
+	freopen("input.txt","r+", stdin);
+	Solution solution;
+	string word1, word2;
+	cin >> word1 >> word2;
+	cout << solution.minDistance(word1, word2);
 	return 0;
 }
