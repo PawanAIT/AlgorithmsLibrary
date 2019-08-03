5837c1a14112dd3d62098ed28feb630c3f586c7d
pawan kumar <pawkumar@microsoft.com>
2019-08-04 01:29:26 +0530

    Cycle in Directed graph

--- Coding/Coding.cpp
+++ Coding/Coding.cpp
@@ -1,16 +1,61 @@
-bool f = true;
-int ans(Node* root) {
-	if (root == NULL)
-		return 0;
-	int l = ans(root->left);
-	int r = ans(root->right);
-	if (abs(l - r) > 1)
-		f = false;
-	return 1 + max(l, r);
+#include "bits/stdc++.h"
+#pragma warning(disable:4996)
+using namespace std;
+const int N = 100;
+vector<int> adj[N];
+int visited[N];
+
+enum { WHITE, GREY, BLACK };
+
+bool CheckCycle(int p) {
+	visited[p] = GREY;
+
+	for (auto i : adj[p]) {
+
+		if (visited[i] == GREY)
+			return true;
+
+		if (visited[i] == WHITE and CheckCycle(i))
+			return true;
+	}
+
+	visited[p] = BLACK;
+	return false;
 }
-bool isBalanced(Node* root)
-{
-	f = true;
-	ans(root);
-	return f;
+
+int main() {
+	//freopen("input.txt", "r+", stdin);
+	
+	int nodes, edges;
+	cin >> nodes >> edges;
+	for (int i = 0; i < edges; i++) {
+		int u, v;
+		cin >> u >> v;
+		adj[u].push_back(v);
+	}
+
+	bool cycleInGraph = false;
+
+	for (int i = 1; i <= nodes; i++) {
+		if (visited[i] == WHITE) {
+			if (CheckCycle(i))
+				cycleInGraph = true;
+		}
+	}
+
+	cout << (cycleInGraph ? "Cycle" : "No Cycle");
+	
+	return 0;
 }
+
+/*
+6
+7
+1 2
+2 3
+1 3
+4 1
+4 5
+5 6
+6 4
+*/
