5f4f4707aaead011d8a8c05b7d5df573f3db2220
pawan kumar <pawkumar@microsoft.com>
2019-08-07 17:35:31 +0530

    Topological sorting using Kahn's algorithm.

--- Coding/Coding.cpp
+++ Coding/Coding.cpp
@@ -1,53 +1,64 @@
 #include "bits/stdc++.h"
+#include "CODING.H"
 #pragma warning(disable:4996)
 using namespace std;
-struct TreeNode {
-	int val;
-	TreeNode* left;
-	TreeNode* right;
-	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+const int N = 100;
+vector<int> adj[N];
+int indegree[N];
+vector<int> TopoOrdering;
 
-};
+void topo(int nodes) {
+	for (int i = 1; i <= nodes; i++) { 
+		for (auto v : adj[i])
+			indegree[v]++;
+	}
+	queue<int> q;
+	for (int i = 1; i <= nodes; i++) {
+		if (indegree[i] == 0) {
+			q.push(i);
+		}
+	}
+	while (!q.empty()) {
+		int u = q.front(); q.pop();
+		TopoOrdering.push_back(u);
+		for (auto v : adj[u]) {
+			indegree[v]--;
+			if (indegree[v] == 0)
+				q.push(v);
+		}
+	}
+}
 
-class Solution {
-public:
-	vector<int> ans;
-	vector<int> distanceK(TreeNode* root, TreeNode* target, int K) {
-		dfs(root, target, K);
-		return ans;
-	}
-	void addnodes(TreeNode* root, int k) {
-		if (root == NULL || k < 0)
-			return;
-		if (k == 0)
-			ans.push_back(root->val);
-		addnodes(root->left, k - 1);
-		addnodes(root->right, k - 1);
+void addEdge(int x, int y) {
+	x++, y++;
+	adj[x].push_back(y);
+}
+
+int main() {
+	addEdge(5, 2);
+	addEdge(5, 0);
+	addEdge(4, 0);
+	addEdge(4, 1);
+	addEdge(2, 3);
+	addEdge(3, 1);
+
+	topo(6);
+
+	for (auto i : TopoOrdering) {
+		cout << i - 1 << " ";
 	}
 
-	int dfs(TreeNode* root, TreeNode* target, int K) {
-		if (root == NULL)
-			return -1;
-		if (root == target) {
-			addnodes(root, K);
-			return 1;
-		}
-		int L = dfs(root->left, target, K);
-		int R = dfs(root->right, target, K);
-		if (L != -1) {
-			if (L == K) {
-				ans.push_back(root->val);
-			}
-			addnodes(root->right, K - L - 1);
-			return L + 1;
-		}
-		if (R != -1) {
-			if (R == K) {
-				ans.push_back(root->val);
-			}
-			addnodes(root->left, K - R - 1);
-			return R + 1;
-		}
-		return -1;
-	}
-};
+	return 0;
+}
+
+/*
+6
+7
+1 2
+2 3
+1 3
+4 1
+4 5
+5 6
+6 4
+*/
