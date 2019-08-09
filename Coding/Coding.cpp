293c7118c969119ab116b546ee2a23d1480b4f59
pawan kumar <pawkumar@microsoft.com>
2019-08-09 22:24:36 +0530

    Dutch national flag algo.

--- Coding/Coding.cpp
+++ Coding/Coding.cpp
@@ -4,20 +4,23 @@
 using namespace std;
 class Solution {
 public:
-	int dp[500];
-	int dfs(int n) {
-		if (n == 0)
-			return 1;
-		if (dp[n]) {
-			return dp[n];
+	void sortColors(vector<int>& nums) {
+		int high = nums.size() - 1, low = 0, mid = 0;
+		while (mid <= high) {
+			switch (nums[mid])
+			{
+			case 0:
+				swap(nums[mid], nums[low]);
+				mid++, low++;
+				break;
+			case 1:
+				mid++;
+				break;
+			case 2:
+				swap(nums[mid], nums[high]);
+				high--;
+				break;
+			}
 		}
-		int ans = 0;
-		for (int i = 1; i <= n; i++) {
-			ans += dfs(i - 1) * dfs(n - i);
-		}
-		return dp[n] = ans;
-	}
-	int numTrees(int n) {
-		return dfs(n);
 	}
 };
