8237114d12183ea87abdbafa9b437120d6982a0e
pawan kumar <pawkumar@microsoft.com>
2019-08-09 13:02:20 +0530

    Binary Tree Level Order Traversal with NULL marker

--- Coding/Coding.cpp
+++ Coding/Coding.cpp
@@ -1,11 +1,41 @@
+#include <cstddef>
+#include <vector>
+#include <queue>
+using namespace std;
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+ 
 class Solution {
 public:
-	TreeNode* invertTree(TreeNode* root) {
-		if (!root)
-			return root;
-		invertTree(root->left);
-		invertTree(root->right);
-		swap(root->left, root->right);
-		return root;
+	vector<vector<int>> levelOrder(TreeNode* root) {
+		vector<vector<int>> ans;
+		vector<int> temp;
+		if (root == NULL)
+			return ans;
+		queue<TreeNode*> Q;
+		Q.push(root);
+		Q.push(NULL);
+		while (!Q.empty()) {
+			auto top = Q.front(); Q.pop();
+			if (top == NULL) {
+				ans.push_back(temp);
+				temp.clear();
+				if (Q.size() > 0) {
+					Q.push(NULL);
+				}
+			}
+			else {
+				temp.push_back(top->val);
+				if (top->left)
+					Q.push(top->left);
+				if (top->right)
+					Q.push(top->right);
+			}
+		}
+		return ans;
 	}
 };
